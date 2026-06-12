a9b3a76ee227dc955506afca12e6d9ea2371b67d
zslari <54786741+zslari@users.noreply.github.com>
2026-06-12 16:13:29 -0700

    Add: python-2d-grid - submission-0

---
+++ Python Coding Interviews/python-2d-grid/submission-0.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+def in_bounds(grid: List[List[int]], r: int, c: int) -> bool:
+    """
+    Get the dimensions of the grid, rows and columns
+
+    Compare dimensions of grid to values of r and c. 
+    If r or c bigger than dimensions grid, return False else True
+
+    """
+
+    rows = len(grid)
+    cols = len(grid[0])
+
+    if r > rows or r < 0:
+        return False
+    
+    if c > cols or c < 0:
+        return False
+    
+    return True
+
+
+
+
+# do not modify below this line
+print(in_bounds([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 0, 0))
+print(in_bounds([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 2, 2))
+print(in_bounds([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 1, 1))
+print(in_bounds([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 4, 3))
+print(in_bounds([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 3, 4))
+print(in_bounds([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 3, -1))
+print(in_bounds([[1, 2, 3], [4, 5, 6], [7, 8, 9]], -1, 3))
